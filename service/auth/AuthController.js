7a948d4416328ac3d8144a93cec7390cfa4a1e0d
José Caballero <angel.caballep@gmail.com>
2018-05-04 17:27:30 -0500

    initial commit

---
+++ service/auth/AuthController.js
@@ -0,0 +1,70 @@
+// AuthController.js
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+
+var User = require('../user/User');
+
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
+
+router.post('/register', function (req, res) {
+
+  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  },
+    (err, user) => {
+      if (err) return res.status(500).send("There was a problem registering the user.")
+      // create a token
+      var token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400
+      });
+      res.status(200).send({ auth: true, token: token });
+    });
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).send('Error on the server.');
+    if (!user) return res.status(404).send('No user found.');
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+    var token = jwt.sign({ id: user_id }, config.secret, {
+      expiresIn: 86400
+    })
+  });
+});
+
+router.get('/me', function (req, res) {
+  var token = req.headers['x-access-token'];
+  if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+
+  jwt.verify(token, config.secret, function (err, decoded) {
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+
+    //res.status(200).send(decoded);
+
+    User.findById(decoded.id, function (err, user) {
+      if (err) return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+
+      res.status(200).send(user);
+    });
+  });
+});
+
+router.get('/test', (req, res) => {
+  res.send('hola ' + req.headers['nombre']);
+});
+
+module.exports = router;
+
+//https://medium.freecodecamp.org/securing-node-js-restful-apis-with-json-web-tokens-9f811a92bb52  <- Continue here
---
+++ service/db.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/users');
+
+//mongodb://<dbuser>:<dbpassword>@ds113200.mlab.com:13200/nodejs-restful
